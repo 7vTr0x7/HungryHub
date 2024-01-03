15430d771d15e3877942ee9e1a404d9db77cc452
7vTr0x7 <7vtrox7@gmail.com>
2024-01-03 18:28:21 +0530

    created body component

--- app.js
+++ app.js
@@ -40,13 +40,45 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://b.zmtcdn.com/data/pictures/chains/9/6507609/9b51e3f85654b9588558b2a11ece5d27_o2_featured_v2.jpg?output-format=webp"
+      />
+      <h3>Cheesiano pizza</h3>
+      <h4>Pizza, Pasta, Momos </h4>
+      <h4>4.4 stars</h4>
+      <h4>20 minutes</h4>
+    </div>
+  );
+};
 
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-
+      <Body />
     </div>
   );
 };
