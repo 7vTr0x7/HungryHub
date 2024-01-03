122893f845ccdb3f9d890a98adc7401f65915ad1
7vTr0x7 <7vtrox7@gmail.com>
2024-01-03 18:27:01 +0530

    created header component

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./logo.png";
 
-const Title = () => (
-  <h1 className="title" id="title">
-    Title
-  </h1>
-);
+/*
 
-const HeadingComponent = () => (
-  <div id="container">
-    <h1 className="header" id="heading">
-      hello world
-    </h1>
-  </div>
-);
+
+# Header
+  - Logo
+  - Nav Items
+# Body
+  - Search
+  - RestaurantContainer
+    - RestaurantCard
+      -img
+      - name of res,star rating , cuisines, delivery time 
+# Footer
+  - Copyrights
+  - Links
+  - Address
+  - Contact
+
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About US</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
