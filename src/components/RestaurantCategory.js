6bb9795e1d0e48aad07e17e34a19d648c234763b
7vTr0x7 <7vtrox7@gmail.com>
2024-01-21 19:01:56 +0530

    accordion

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,20 +1,28 @@
+import { useState } from "react";
 import down from "../../utils/down.png";
+import up from "../../utils/up.png";
 import ItemList from "./ItemList";
 const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState(false);
 
+  const handleClick = () => {
+    showItems ? setShowItems(false) : setShowItems(true);
+  };
   return (
     <div>
       <div className="w-6/12 mx-auto my-5 p-4 bg-gray-50 shadow-lg  rounded-lg">
-        <div className="flex justify-between">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}>
           <span className="font-bold text-lg">
-            {data.title} ({data.itemCards.length}){" "}
+            {data.title} ({data.itemCards.length})
           </span>
           <span>
-            <img src={down} alt="down" className="h-4" />
+            <img src={showItems ? up : down} alt="down" className="h-4" />
           </span>
         </div>
 
-        <ItemList items={data.itemCards} />
+        {showItems ? <ItemList items={data.itemCards} /> : " "}
       </div>
     </div>
   );
